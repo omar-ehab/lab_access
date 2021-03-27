100daa1a0a6422b0019acb8d5467cc3dbe5e4999
omar-ehab <topdollar57@gmail.com>
2021-03-28 01:42:26 +0200

    update route verbs

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const labAccessController = require('../controllers/labAccessController');
 
-router.get('/store_access_log', labAccessController.store_access);
+router.post('/store_access_log', labAccessController.store_access);
 
 router.get('/labs', labAccessController.get_distincet_labs);
 
-router.post('/download_excel/:lab_id', labAccessController.download_excel);
+router.get('/download_excel/:lab_id', labAccessController.download_excel);
 
 module.exports = router;
